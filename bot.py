9428a1ba6b0b1becde04665930f4c90f221004a7
AlanTheBlank <alanoreilly@outlook.ie>
2020-06-09 18:23:05 +0100

    Updated bot.py and requirments.txt

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ import requests
 import pytesseract
 from bs4 import BeautifulSoup
 import imageio
+import cv2
+import instaloader
+
+IL = None
 
 bot = discord.Client(command_prefix="\\", status=discord.Status.online)
 f = open('api.json')
@@ -21,6 +25,7 @@ async def on_message(msg):
     if(msg.author.id != bot.user.id):
         chan = bot.get_channel(msg.channel.id)
         if(msg.embeds):
+            print("Embed")
             for attachment in msg.embeds:
                 url = attachment.url
                 id = url.rsplit("-",1)[1]
@@ -28,12 +33,17 @@ async def on_message(msg):
                 beaut = BeautifulSoup(r.text, "html.parser")
                 for img in beaut.find_all("img"):
                     if(id in img.get("src")):
+                        print(img.get("src"))
                         with open("temp/" + img.get("src").rsplit("/", 1)[1][:9], "wb+") as f:
                             f.write(requests.get(img.get("src")).content)
-                        if(hasText("more", "temp/" + img.get("src").rsplit("/", 1)[1][:9])):
+                        if(hasText("temp/" + img.get("src").rsplit("/", 1)[1][:9])):
+                            await msg.add_reaction('\U00002705')
                             async with chan.typing():
                                 await chan.send(file=File("memes/" + random.choice(os.listdir("memes"))))
+                        else:
+                            await msg.add_reaction('\U0000274C')
         elif(msg.attachments):
+            print("Attachment")
             for attch in msg.attachments:
                 url = attch.url
                 filetype = url.rsplit(".", 1)[1]
@@ -45,25 +55,37 @@ async def on_message(msg):
                         f.write(r.content)
                         await msg.add_reaction('\U0001f44d')
         else:
+            print(msg)
             print("Nothing of interest")
+    for x in os.listdir("temp"):
+        os.remove("temp/" + x)
+
 
 @bot.event
 async def on_ready():
+    global IL
     if(not os.path.isdir("temp")):
         os.mkdir("temp")
     print("Started")
+    IL = instaloader.Instaloader(dirname_pattern="memes", download_video_thumbnails=False, download_comments=False, download_geotags=False)
+    for p in instaloader.Hashtag.from_name(IL.context, "yeetmemes").get_posts():
+        IL.download_post(p, target="#yeetmemes")
     await bot.change_presence(activity=discord.CustomActivity(name="I will steal your memes"))
 
-def hasText(term, file):
+def hasText(file):
+    terms = ['MO', 'OR', 'RE', 'MOR', 'ORE', 'MORE']
     img = imageio.get_reader(file)
-    count = 3
+    count = 0
     for frame in img:
-        if(count == 0):
-            if(term.upper() in pytesseract.image_to_string(frame).upper()):
+        cv2.imwrite("temp/temp.jpg", frame)
+        im = cv2.imread("temp/temp.jpg")
+        grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        out = pytesseract.image_to_string(grey, config='--psm 6')
+        for term in terms:
+           if(term in out.upper()):
+                print("has text")
                 return True
-            count = 3
-        else:
-            count -= 1
+    print("No text")
     return False
 
 bot.run(api_keys["discord"])
